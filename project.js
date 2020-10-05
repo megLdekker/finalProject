7c9ece8bfbbeb16e5477147f21e22402951cfec8
Margaret Dekker <leighdekker@Margarets-MacBook-Air.local>
2020-10-04 23:01:23 -0400

    gay

---
+++ project.js
@@ -0,0 +1,8 @@
+var setBanner=function(message)
+{
+    d3.select("#banner")
+    .text(message)
+}
+var successFCN = function()
+var projectPromise=d3.csv("Data/LGBTPopulation.csv")
+projectPromise.then(succ)
